531b3fd6bb64e4a118cd706a033b6438700b9f96
Soham17102011 <115869174+Soham17102011@users.noreply.github.com>
2022-11-01 17:06:31 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-song1 = "";
-song2 = "";
+song1 = "music.mp3";
+song2 = "music2.mp3";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+songPlayed = "";
 
 function preload() {
     song1 = loadSound("music.mp3")
@@ -34,9 +36,28 @@ function gotPoses(results) {
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
     }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500)
+    songPlayed = song1.isPlaying();
+    if (scoreLeftWrist > 0.2) {
+        fill('#FC6A03');
+        stroke('#FC6A03');
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if (songPlayed = false) {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song Playing: Peter Pan";
+        }
+    }
+}
+
+function play() {
+    song1.play();
+    song1.setVolume(1);
+
 }
